11f8cf07000a883c24785aed3accddb138ee1491
Octave28 <enockakpacho18@gmail.com>
2024-05-13 16:23:20 -0400

    MISE À JOUR

--- OS/script1.py
+++ OS/script1.py
@@ -1,26 +1,23 @@
 import os 
 import calendar
 import locale
+import argparse
 
 # Fonction pour créer des dossiers et des fichiers
 def generer_dossier_fichiers(path_dossier : str) -> None:
     """
-    Crée une structure de dossiers pour les 12 mois en français avec 15 fichiers numérotés dans chaque dossier.
+    Crée un répertoire dont le chemin est passé en paramètre puis un dossier pour chacun 12 mois en français avec 15 fichiers de logs numérotés dans chaque dossier.
 
-    :param reportoire : Chemin du répertoire de base où créer la structure.
+    Paramètre : Chemin du répertoire parent où créer la structure.
     """
     try:
         # Création du répertoire parent
         # Nom du dossier à créer
         nom_dossier = "RepertoireParent"
 
-        # Chemin complet du nouveau dossier
+        # Chemin complet du répertoire
         chemin_nouveau_dossier = os.path.join(path_dossier, nom_dossier)
         os.makedirs(chemin_nouveau_dossier, exist_ok=False)
-
-        # if not repertoireParent.exists():
-        #     repertoireParent.mkdir(parents=True, exist_ok=True)
-        #     #print(f"Répertoire créé avec succes : {repertoire}")
 
         # Noms des mois en français
         locale.setlocale(locale.LC_TIME, 'fr_FR')
@@ -28,32 +25,40 @@ def generer_dossier_fichiers(path_dossier : str) -> None:
         print(moisdelannee)
 
 
-        # Création des sous-dossiers et fichiers
+        # Création des sous-dossiers puis de fichiers
+
         for mois in range(len(moisdelannee)):
-            # sous_repertoire = repertoireParent/moisdelannee[mois]
-            # if not sous_repertoire.exists():
-            #     sous_repertoire.mkdir()
             # Obtenir le nom du mois
             nom_mois = (moisdelannee[mois])
             # Ajouter un zéro devant les mois de 1 à 9
-            mois_str = str(mois+1).zfill(2)
+            numero_mois_str = str(mois+1).zfill(2)
             # Créer le répertoire pour le mois
-            mois_path = os.path.join(chemin_nouveau_dossier, f"{mois_str}_{nom_mois}")
-            os.makedirs(mois_path, exist_ok=True)
+            mois_path = os.path.join(chemin_nouveau_dossier, f"{numero_mois_str}-{nom_mois}")
+            os.makedirs(mois_path, exist_ok=False)
                 
             # Création des fichiers dans le sous-dossier pour chaque mois
             for i in range(1, 16):
                 # Ajouter un zéro devant les nombres de 1 à 9
-                fichier_num = str(i).zfill(2)
-                nom_fichier = f"log{fichier_num}.txt"
+                numero_fichier = str(i).zfill(2)
+                nom_fichier = f"log{numero_fichier}.txt"
                 chemin_fichier = os.path.join(mois_path, nom_fichier)
                 os.system(f"echo. > {chemin_fichier}")
-    except Exception as e:
-        print(e)
 
+    except PermissionError:
+        print("Erreur : vous n'êtes pas autorisé à lire ou à écrire dans ce dossier.")
+    except FileNotFoundError:
+        print("Erreur : le chemin spécifié n'existe pas.")
+    except OSError:
+        print(f"Erreur liée au système d'exploitation : {e}")
+    except Exception as e:
+        print(f"Une erreur inconnue s'est produite : {e}")
+     
 
 # Section principale du script
 if __name__ == "__main__":
-    generer_dossier_fichiers("C:/Users/abelc/Desktop/Python")
+    parser = argparse.ArgumentParser(description="Recherche d'un mot dans un répertoire.")
+    parser.add_argument("chemin_dossier", type=str, help="Chemin du dossier central")
+    args = parser.parse_args()
+    generer_dossier_fichiers(args.chemin_dossier)
 
 
--- OS/script2.py
+++ OS/script2.py
@@ -5,23 +5,20 @@ def rechercher_mot_dans_dossier(mot_a_chercher, chemin_dossier, chemin_resultats
     try:
         with open(chemin_resultats, 'w') as fichier_resultats:
             # Parcourir tous les fichiers et sous-dossiers du dossier spécifié
-            for dossier_racine, _, fichiers in os.walk(chemin_dossier):
+            for dossier_racine, dossiers, fichiers in os.walk(chemin_dossier):
                 for nom_fichier in fichiers:
                     chemin_fichier = os.path.join(dossier_racine, nom_fichier)
-                    # Lire chaque ligne du fichier et rechercher le mot
+                    # Ouvrir le fichier en mode lecture
                     with open(chemin_fichier, 'r') as fichier:
-                        for numero_ligne, ligne in enumerate(fichier, 1):
+                        # Parcourir chaque ligne du fichier
+                        numero_ligne = 1
+                        for ligne in fichier:
                             if mot_a_chercher in ligne:
-                                # Écrire le résultat dans le fichier de résultats
                                 fichier_resultats.write(f"{chemin_fichier}, ligne {numero_ligne}: {ligne.strip()}\n")
-    except PermissionError:
-        print("Erreur : vous n'êtes pas autorisé à lire le dossier ou à écrire le fichier de résultats.")
-    except FileNotFoundError:
-        print("Erreur : le chemin spécifié n'existe pas.")
-    except OSError as e:
-        print(f"Erreur liée au système d'exploitation : {e}")
+                            numero_ligne += 1
+
     except Exception as e:
-        print(f"Une erreur inconnue s'est produite : {e}")
+        print(e)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Recherche d'un mot dans un répertoire.")
@@ -32,3 +29,4 @@ if __name__ == "__main__":
 
     # Exécuter la recherche
     rechercher_mot_dans_dossier(args.mot, args.chemin_dossier, args.chemin_resultats)
+    rechercher_mot_dans_dossier("police", "c:/Users/Utilisateur/Downloads", "c:/Users/Utilisateur/Downloads/script1.py")
